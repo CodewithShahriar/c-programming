b26cdcf802748a933c2ea9a204299e5a2a7503a6
CodewithShahriar <abid.shahriar.23.com>
2024-01-02 02:47:29 +0600

    hello wolrd

--- hello.c
+++ hello.c
@@ -2,17 +2,6 @@
 
 int main()
 {
-    int playerScore ;
-
-    printf("playerScore: %d\n", playerScore);
-    
-    playerScore = 93 ;
-
-    int player = 95 ;
-
-    
-    printf("value of playerScore after initialization: %d\n", playerScore);
-    printf("value of player: %d", player);
-
+    printf("Hello world!");
     return 0;
 }
