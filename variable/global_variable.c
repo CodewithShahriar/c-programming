11cf4ba32266b6f29bccce0a63b941be47f2e971
CodewithShahriar <abid.shahriar.23.com>
2024-01-02 03:22:07 +0600

    variable types

---
+++ variable/global_variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+    int playerScore = 5 ;
+    int matchScore = 15 ;
+
+int main()
+{
+    
+    
+
+    printf("%d", playerScore + matchScore);
+
+    return 0;
+}
---
+++ variable/local_variable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int playerScore = 10 ;
+    int matchScore = 12 ;
+
+   
+
+    
+    
+    printf("%d", playerScore + matchScore);
+
+    return 0;
+}
