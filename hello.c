75c4bf82a49f2df46339a9e49366beaaea1ab7e7
CodewithShahriar <abid.shahriar.23.com>
2024-01-02 02:44:20 +0600

    first commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int playerScore ;
+
+    printf("playerScore: %d\n", playerScore);
+    
+    playerScore = 93 ;
+
+    int player = 95 ;
+
+    
+    printf("value of playerScore after initialization: %d\n", playerScore);
+    printf("value of player: %d", player);
+
+    return 0;
+}
