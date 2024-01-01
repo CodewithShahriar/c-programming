4268f9a8feddb898c465655d56ed343ac84d0884
CodewithShahriar <abid.shahriar.23.com>
2024-01-02 02:50:53 +0600

    variable

---
+++ variable.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // declaration with definition
+    int playerScore ;
+
+    printf("playerScore: %d\n", playerScore);
+    
+    // initialization
+    playerScore = 93 ;
+
+
+    // declaration + definition + initialization
+    int player = 95 ;
+
+    
+    printf("value of playerScore after initialization: %d\n", playerScore);
+    printf("value of player: %d", player);
+
+    return 0;
+}
