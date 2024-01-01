eca37b7524c10bfadddeab4617fb4f35556910ce
CodewithShahriar <abid.shahriar.23.com>
2024-01-02 03:55:46 +0600

    data types

---
+++ data_types.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 18;
+    char section = 'A';
+    float salary = 10000.89;
+    double bonousSalary = 2000.15483;
+
+    printf("Age:%d\n", age);
+    printf("section:%c\n", section);
+    printf("salary:%f\n", salary);
+    printf("bonosSalary: %lf\n", bonousSalary);
+    return 0;
+}
+
+
+
+
+// #include <stdio.h>
+// int main() {
+//   short a;
+//   long b;
+//   long long c;
+//   long double d;
+
+//   printf("size of short = %d bytes\n", sizeof(a));
+//   printf("size of long = %d bytes\n", sizeof(b));
+//   printf("size of long long = %d bytes\n", sizeof(c));
+//   printf("size of long double= %d bytes\n", sizeof(d));
+//   return 0;
+// }
