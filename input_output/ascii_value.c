15037740c247888e3653418b3c62b50952d148f7
CodewithShahriar <abid.shahriar.23.com>
2024-01-09 01:18:46 +0600

    input_output

---
+++ input_output/ascii_value.c
@@ -0,0 +1,23 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num ;
+    float num2;
+    double num3;
+    char name;
+
+    
+
+    printf("Enter Character: ");
+    scanf(" %c", &name);
+
+    printf("Interger Number: %d\n", num);
+    printf("Float Number: %f\n", num2);
+    printf("Double Number: %lf\n", num3);
+    printf("Character: %d\n", name);
+
+    return 0;
+}
