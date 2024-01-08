1439d52e387f4ee7b9c49150c73d4b73ad47676d
CodewithShahriar <abid.shahriar.23.com>
2024-01-09 01:22:12 +0600

    multiple input

---
+++ input_output/multiple_value.c
@@ -0,0 +1,21 @@
+// input A output 65
+
+#include <stdio.h>
+
+int main()
+{
+    int num ;
+    float num2;
+    double num3;
+    char name;
+
+    
+
+    printf("Enter Character and num: ");
+    scanf(" %c %d", &name, &num);
+
+    
+    printf("Character: %c Number: %d", name,num);
+
+    return 0;
+}
